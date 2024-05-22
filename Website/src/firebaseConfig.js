738853cbf5d58e5c9f159ac2371cffa3c34cb8ce
justinmitchell341 <justin.mitchell341@gmail.com>
2024-05-22 15:45:20 -0700

    Revert "test"
    This reverts commit a38fce0ad2b2daacd8bc958b7b0c24f69fb90535.

--- Website/src/firebaseConfig.js
+++ Website/src/firebaseConfig.js
@@ -1,8 +1,9 @@
-// src/firebaseConfig.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // Add Firestore import
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC-qHk1WOIB8R3wWtaUzJ-cQX7aeEttsw0",
   authDomain: "team404-a5176.firebaseapp.com",
@@ -15,7 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+const db = getFirestore(app); // Initialize Firestore
 
-export { db, auth };
+export { db }; // Export the Firestore database
